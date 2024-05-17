3bab4f5ebcb35096471461132b998e93ba8c6477
MassimoRomano <romano93m@gmail.com>
2024-05-17 17:20:16 +0200

    add route

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Character;
 use Illuminate\Http\Request;
 
 class CharacterController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $characters = Character::orderBy('name')->get();
+        return view('guests.characters', compact('characters'));
+    }
 }
