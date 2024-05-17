3addba9656caa2ba2fa9cd07762551b80c180d6b
devitoangelo <angelodevito4@gmail.com>
2024-05-17 16:53:00 +0200

    layout characters

--- resources/views/guests/characters.blade.php
+++ resources/views/guests/characters.blade.php
@@ -4,15 +4,15 @@
 @section('content')
     <div class="container">
         <div class="row">
-            @foreach ($characters as $character)  
+            @foreach ($characters as $character)
                 <div class="col-4 my-3">
                     <div class="card">
                         <div class="card-body">
-                            <h2 class="card-title">Nome: {{$character->name}}</h2>
-                            <h5 class="card-text">Descrizione: {{$character->description}}</h5>
-                            <h5 class="card-text">Atk:{{$character->attack}}</h5>
-                            <h5 class="card-text">Df: {{$character->defense}}</h5>                       
-                            <h5 class="card-text">Sp: {{$character->speed}}</h5>
+                            <h2 class="card-title">Nome: {{ $character->name }}</h2>
+                            <h5 class="card-text">Descrizione: {{ $character->description }}</h5>
+                            <h5 class="card-text">Atk:{{ $character->attack }}</h5>
+                            <h5 class="card-text">Df: {{ $character->defense }}</h5>
+                            <h5 class="card-text">Sp: {{ $character->speed }}</h5>
                         </div>
                     </div>
                 </div>
@@ -20,4 +20,3 @@
         </div>
     </div>
 @endsection
-
