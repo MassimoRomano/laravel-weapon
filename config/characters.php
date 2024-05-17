2282a4baf9f340bdf7ab3a9296c13da0c27dfddc
MassimoRomano <romano93m@gmail.com>
2024-05-17 16:16:16 +0200

    another fix on table db

--- config/characters.php
+++ config/characters.php
@@ -6,35 +6,35 @@ return  [
             'name' => 'Massimo',
             'description' => 'Pelato',
             'attack' => '80',
-            'defence' => '70',
+            'defense' => '70',
             'speed' => '50'
         ],
         [
             'name' => 'Giorgia',
             'description' => 'Ansia',
             'attack' => '90',
-            'defence' => '70',
+            'defense' => '70',
             'speed' => '50'
         ],
         [
             'name' => 'Matteo',
             'description' => 'Occhiali',
             'attack' => '60',
-            'defence' => '40',
+            'defense' => '40',
             'speed' => '30'
         ],
         [
             'name' => 'Angelo',
             'description' => 'Ciuffo',
             'attack' => '70',
-            'defence' => '40',
+            'defense' => '40',
             'speed' => '40'
         ],
         [
             'name' => 'Arya',
             'description' => 'Tatuagio',
             'attack' => '40',
-            'defence' => '30',
+            'defense' => '30',
             'speed' => '30'
         ]
     ]
