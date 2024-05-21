5c39e751fbd351cb56c009b14d58e9ed1d0231f8
MatteoStrazzera <mattestrazz@gmail.com>
2024-05-21 16:09:39 +0200

    Git/Auth - Allineamento con auth

--- resources/views/guests/characters.blade.php
+++ resources/views/guests/characters.blade.php
@@ -2,17 +2,17 @@
 
 
 @section('content')
-    <div class="container my-5">
+    <div class="container">
         <div class="row">
-            @foreach ($characters as $character)  
+            @foreach ($characters as $character)
                 <div class="col-4 my-3">
                     <div class="card">
                         <div class="card-body">
-                            <h2 class="card-title mb-3">Nome: {{$character->name}}</h2>
-                            <h5 class="card-text"><strong>Descrizione:</strong> {{$character->description}}</h5>
-                            <h5 class="card-text"><strong>Atk:</strong> {{$character->attack}}</h5>
-                            <h5 class="card-text"><strong>Df:</strong> {{$character->defense}}</h5>                       
-                            <h5 class="card-text"><strong>Sp:</strong> {{$character->speed}}</h5>
+                            <h2 class="card-title mb-3">Nome: {{ $character->name }}</h2>
+                            <h5 class="card-text"><strong>Descrizione:</strong> {{ $character->description }}</h5>
+                            <h5 class="card-text"><strong>Atk:</strong> {{ $character->attack }}</h5>
+                            <h5 class="card-text"><strong>Df:</strong> {{ $character->defense }}</h5>
+                            <h5 class="card-text"><strong>Sp:</strong> {{ $character->speed }}</h5>
                             <a class="btn btn-primary my-3" href="{{ route('guests.show_character', $character) }}">Show</a>
                         </div>
                     </div>
@@ -21,4 +21,3 @@
         </div>
     </div>
 @endsection
-
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CharacterController;
 use App\Http\Controllers\WeaponController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +17,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'home')->name('home');
+
+// Weapons
 Route::get('/weapon', [WeaponController::class, 'weapon'])->name('weapon');
-Route::get('/weapon{weapon}',[WeaponController::class, 'show'])->name('guests.show_weapon');
+Route::get('/weapon{weapon}', [WeaponController::class, 'show'])->name('guests.show_weapon');
+
+// Characters
 Route::get('/character', [CharacterController::class, 'characters'])->name('characters');
-Route::get('/character{character}',[CharacterController::class, 'show'])->name('guests.show_character');
+Route::get('/character{character}', [CharacterController::class, 'show'])->name('guests.show_character');
+
+// Laravel Breeze Auth
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
