864eaa3d0f251e87cf41f4e19f8b7fa11b105c7f
MassimoRomano <romano93m@gmail.com>
2024-05-29 16:04:53 +0200

    fix error

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\CharacterController;
-use App\Http\Controllers\WeaponController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WeaponController;
+use App\Http\Controllers\CharacterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,24 +17,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'home')->name('home');
-
 // Weapons
 Route::get('/weapon', [WeaponController::class, 'weapon'])->name('weapon');
 Route::get('/weapon/{weapon}', [WeaponController::class, 'show'])->name('guests.show_weapon');
-
 // Characters
 Route::get('/character', [CharacterController::class, 'characters'])->name('characters');
 Route::get('/character/{character}', [CharacterController::class, 'show'])->name('guests.show_character');
-
 // Laravel Breeze Auth
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
 require __DIR__ . '/auth.php';
