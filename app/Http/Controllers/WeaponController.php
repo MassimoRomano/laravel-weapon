24b4cff3d59fb375a87b625ed1d1b41ed35e0355
GioMench <giorgiaglm@gmail.com>
2024-06-03 16:42:52 +0200

    Aligned WeaponController to be Resource for CRUD

--- app/Http/Controllers/WeaponController.php
+++ app/Http/Controllers/WeaponController.php
@@ -9,13 +9,63 @@ class WeaponController extends Controller
 {
     public function weapon()
     {
-
         $weapons = Weapon::orderBy('name')->get();
         return view('guests.weapon', compact('weapons'));
     }
 
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
     public function show(Weapon $weapon)
     {
         return view('guests.show_weapon', compact('weapon'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
