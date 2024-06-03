21ba9d16e2b1b2870da7d81e75bea38255060fc3
MassimoRomano <romano93m@gmail.com>
2024-06-03 16:37:21 +0200

    Aligned Controller Characters to be Resource for do the CRUD

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -7,14 +7,67 @@ use Illuminate\Http\Request;
 
 class CharacterController extends Controller
 {
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $characters = Character::orderBy('name')->get();
+        return view('guests.characters', compact('characters'));
+    }
+
     public function characters()
     {
         $characters = Character::orderBy('name')->get();
         return view('guests.characters', compact('characters'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
     public function show(Character $character)
     {
         return view('guests.show_character', compact('character'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
